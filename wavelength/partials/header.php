15b678531db70142b7a31f12a2b5eb8df383bc8c
babanbodi <jameelabdullahi57@gmail.com>
2026-09-25 20:33:01 +0100

    Updated project

--- wavelength/partials/header.php
+++ wavelength/partials/header.php
@@ -7,7 +7,7 @@ $pageTitle = $pageTitle ?? 'Wavelength';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle) ?> - Wavelength</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=4">
 </head>
 <body>
 <header class="site-header">
